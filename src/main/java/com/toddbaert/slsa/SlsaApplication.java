f388abf2967abc72f675d577ba9d38881e3adabe
Todd Baert <todd.baert@dynatrace.com>
2023-06-21 13:12:42 -0400

    add code
    Signed-off-by: Todd Baert <todd.baert@dynatrace.com>

---
+++ src/main/java/com/toddbaert/slsa/SlsaApplication.java
@@ -0,0 +1,13 @@
+package com.toddbaert.slsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlsaApplication.class, args);
+	}
+
+}
