087633e17411f5fd316eaaf2e518f8d6fe505b95
Todd Baert <todd.baert@dynatrace.com>
2023-06-21 13:12:42 -0400

    add code
    Signed-off-by: Todd Baert <todd.baert@dynatrace.com>

---
+++ src/test/java/com/toddbaert/slsa/SlsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toddbaert.slsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
